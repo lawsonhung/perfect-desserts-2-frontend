ab6e3f7e48a45cd48d467f2b9a2dd37770d34d51
lawsonhung <lawsonhung@yahoo.com>
2020-05-22 20:09:06 -0400

    Successfully log in and sign up locally

--- src/App.js
+++ src/App.js
@@ -1,36 +1,76 @@
 import React, { Component } from 'react';
 import ProfilePage from './ProfilePage';
+import HomePage from './HomePage';
 import LoginPage from './LoginPage';
+// eslint-disable-next-line
 import SignUpPage from './SignUpPage';
+import FourOhFourPage from './FourOhFourPage';
+import MessagePage from './MessagePage';
+import { Switch, Route, withRouter } from 'react-router-dom';
 
-export default class App extends Component {
+class App extends Component {
+
   state = {
-    page: 'login'
-  }
-
-  redirect = (page) => {
-    // Shortcut if key and value are same name
-    // this.setState({ page: page })
-    this.setState({ page })
+    username: '',
+    // password: ''
   }
 
   componentDidMount() {
     if (localStorage.token) {
-      this.redirect('profile');
+      // Fetch profile
+      fetch('http://localhost:3000/profile',{
+      headers: {
+        'Authorization': `Bearer ${localStorage.token}`
+      }
+    })
+    .then(res => res.json())
+    .then(user => this.setState({username: user.username}))
+    } else {
+      // This is done with withRouter technology. Using HOC, you can user router props
+      this.props.history.push('/login');
     }
   }
 
   render() {
+    console.log(this.props);
 
-    switch (this.state.page) {
-      case 'login':
-        return <LoginPage redirect={this.redirect}/>
-      case 'signup':
-        return <SignUpPage/>
-      case 'profile':
-        return <ProfilePage redirect={this.redirect}/>
-      default:
-        return <LoginPage/>
-    }
+    return (
+      <Switch>
+        
+        {/* <Route 
+          path={'/profile'} 
+          component={ProfilePage} /> */}
+          {/* Same as below, but below allows you to pass props */}
+        <Route path={'/profile'} 
+          render={(routerProps) => <ProfilePage 
+            username={this.state.username} 
+            routerProps={routerProps} />} />
+
+        <Route path={'/login'} 
+          render={(routerProps) => <LoginPage 
+            username={this.state.username}
+            password={this.state.password}
+            routerProps={routerProps} />} />
+
+        <Route path={'/signup'}
+          render={(routerProps) => <SignUpPage
+            username={this.state.username}
+            password={this.state.password}
+            routerProps={routerProps} />} />
+
+        <Route path={'/messages/:text'} 
+          render={(routerProps) => <MessagePage 
+          username={this.state.username}
+          // {...routerProps} is syntactic sugar for routerProps={routerProps}. They are both the same
+          {...routerProps} />} />
+
+        <Route path={'/'} component={HomePage} />
+
+        <Route component={FourOhFourPage} />
+
+      </Switch>
+    )
   }
 }
+
+export default withRouter(App);
--- src/LoginPage.js
+++ src/LoginPage.js
@@ -25,20 +25,30 @@ export default class LoginPage extends Component {
     .then(data => {
       if (data.token) {
         localStorage.token = data.token;
+        console.log(this);
+        // debugger
+        // this.props.redirect('profile');
+        this.props.routerProps.history.push('/profile');
       }
     });
   }
+
+  handleClick = () => {
+    this.props.routerProps.history.push('/signup');
+  }
   
   render() {
+    console.log("Login props: ", this.props);
+
     return (
       <div>
-        {/* <button>Switch to Signup. Don't have an account? Sign up here</button> */}
         <h1>Log in please!</h1>
         <form onSubmit={this.handleSubmit}>
           <input onChange={this.handleChange} value={this.state.username} type="text" name="username"/>
           <input  onChange={this.handleChange} value={this.state.password} type="password" name="password"/>
           <input type="submit" value="Log in" />
         </form>
+        <button onClick={this.handleClick}>Don't have an account yet? Click here to create one</button>
       </div>
     );
   }
